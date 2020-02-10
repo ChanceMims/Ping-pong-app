8794be6ed5a2744d6113560e3a462c32f58a23ff
Chance Mims <chance@Chances-MacBook-Air.local>
2020-02-10 11:46:17 -0600

    created home page, added fun gifs, could work on style

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello world')
+    const HOMEGIFS = ['https://media.giphy.com/media/lIv7H1vUVdaH6/giphy.gif', 'https://media.giphy.com/media/alMx8lxNgQv6g/giphy.gif', 'https://media.giphy.com/media/R0JQZRxyTmhTG/giphy.gif', 'https://media.giphy.com/media/aWtm6hVSJmRt6/giphy.gif', 'https://media.giphy.com/media/11rBFQqIe47Lck/giphy.gif', 'https://media.giphy.com/media/aDOiaVWvvZzdC/giphy.gif', 'https://media.giphy.com/media/Wyaq1InVpNDEc/giphy.gif', 'https://media.giphy.com/media/kjReZ2vUmMBfG/giphy.gif']
+    setGif();
+
+    function setGif(){
+        const homeGifIndex = Math.floor(Math.random() * HOMEGIFS.length);
+        const homeGif = document.getElementById('homeGif');
+        homeGif.setAttribute('src', HOMEGIFS[homeGifIndex]);
+    }
 })
